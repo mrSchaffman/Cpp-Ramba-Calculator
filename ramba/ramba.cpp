0bd7dc41b664e3282de5b6b5b9d381ccb4e06a8b
Barth. Feudong <61944369+mrSchaffman@users.noreply.github.com>
2022-06-20 15:18:12 +0200

    Ramba: main Copyright update

--- ramba/ramba.cpp
+++ ramba/ramba.cpp
@@ -22,6 +22,9 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Ramba  Copyright (C) 2022  Barth. Feudong\n"
+        << "This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\n"
+        << "This is free software, and you are welcome to redistribute it\n"
+        << "under certain conditions; type `show c' for details.\n\n";
 }
 
