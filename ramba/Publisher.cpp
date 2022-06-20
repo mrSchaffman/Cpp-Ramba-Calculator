c5b0555e84da5243f83be667a3b04dfcfb738b32
Mr.Schaffman <61944369+mrSchaffman@users.noreply.github.com>
2022-06-20 20:42:07 +0200

    Utilities: Publisher, Observer, Exception, Event implemented

--- ramba/Publisher.cpp
+++ ramba/Publisher.cpp
@@ -25,6 +25,7 @@
 #include<memory>
 #include<sstream>
 #include"Exception.h"
+#include<algorithm>
 
 using std::unordered_map;
 
@@ -33,37 +34,99 @@ namespace utility
     class Publisher::PublisherImpl
     {
     public:
+		using ObserverName = const std::string;
+		using EventName = const std::string;
+
         PublisherImpl() = default;
         ~PublisherImpl() = default;
 
-        void subscribe(const string& eventName, unique_ptr<Observer> observer);
-        void unsubscribe(const string& eventName, const string& observerName);
-        void notify(const string& eventName, shared_ptr<Event>event);     // push semantic
+        void subscribe(EventName& eventName, unique_ptr<Observer> observer);
+        void unsubscribe(EventName& eventName, ObserverName& observerName);
+        void notify(EventName& eventName, shared_ptr<Event>event);     // push semantic
     private:
-        using Observers = unordered_map<string, unique_ptr<Observer>>;
-        using Events = unordered_map<string, Observers>;
 
-        Events m_events;
+        using ObserversByEvent = unordered_map<ObserverName, unique_ptr<Observer>>;
+        using Observers = unordered_map<EventName, ObserversByEvent>;
+
+		Observers m_observers;
     };
 
-    void Publisher::PublisherImpl::subscribe(const std::string& eventName, std::unique_ptr<Observer> observer)
+    void Publisher::PublisherImpl::subscribe(EventName& eventName, std::unique_ptr<Observer> observer)
     {
+		auto ptr = m_observers.find(eventName);
+		if (ptr == std::end(m_observers))
+		{
+			std::ostringstream oss;
+			oss << "Event with name '" << eventName << "' not supported";
+			throw Exception(oss.str());
+
+		}
+		else
+		{
+			auto iter = m_observers[eventName].insert(std::make_pair(observer->getName(), std::move(observer)));
+			if (!iter.second)
+			{
+				std::ostringstream oss;
+				oss << "Observer '" << observer->getName() << "' is already registered";
+				throw Exception(oss.str());
+
+			}
+				
+		}
     }
 
-    void Publisher::PublisherImpl::unsubscribe(const string& eventName, const string& observerName)
+    void Publisher::PublisherImpl::unsubscribe(EventName& eventName, ObserverName& observerName)
     {
+		auto count = m_observers.count(eventName);
+		if (count)
+		{
+			auto found = m_observers[eventName].erase(observerName);
+			if (!found)
+			{
+				std::ostringstream oss;
+				oss << "Observer '" << observerName << "' not found registered";
+				throw Exception(oss.str());
+
+			}
+		}
+		else
+		{
+			std::ostringstream oss;
+			oss << "Event with name '" << eventName << "' not supported";
+			throw Exception(oss.str());
+
+		}
     }
 
-    void Publisher::PublisherImpl::notify(const string& eventName, shared_ptr<Event> event)
+    void Publisher::PublisherImpl::notify(EventName& eventName, shared_ptr<Event> event_)
     {
+		auto ptr = m_observers.find(eventName);
+		if (ptr != std::end(m_observers))
+			std::for_each(std::begin(ptr->second), std::end(ptr->second), [](auto & observer) { observer.second->notify(event_); });
+		else
+		{
+			std::ostringstream oss;
+			oss << "Event with name '" << eventName << "' not supported";
+			throw Exception(oss.str());
+
+		}
+
     }
+	void Publisher::subscribe(const string& eventName, unique_ptr<Observer> observer)
+	{
+		publisherImpl->subscribe(eventName, std::move(observer));
+	}
 
     void Publisher::unsubscribe(const std::string& eventName, const std::string& observerName)
     {
+		publisherImpl->unsubscribe(eventName, observerName);
+
     }
 
     void Publisher::notify(const std::string& eventName, std::shared_ptr<Event> event)
     {
+		publisherImpl->notify(eventName, event);
+
     }
 
 }
