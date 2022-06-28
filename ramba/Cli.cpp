b59a86e00079d9651e245aebefcd2372a04c74e1
Barth. Feudong <61944369+mrSchaffman@users.noreply.github.com>
2022-06-29 00:04:07 +0200

    Ramba: ui-> CLI Class definition

---
+++ ramba/Cli.cpp
@@ -0,0 +1,31 @@
+/*
+    Copyright (C) 2022  Barth.Feudong
+    Author can be contacted here: <https://github.com/mrSchaffman/Cpp-Ramba-Calculator>
+
+    This file is part of the Ramba Command Line Calculator project.
+
+    ramba is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ramba is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+#include "Cli.h"
+namespace ui
+{
+    void Cli::inputUserMessage(const std::string& msg)
+    {
+    }
+    void Cli::stackChanged()
+    {
+    }
+}
